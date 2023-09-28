62039e3208268c38f072cb44e34c1f91606249f7
T x <emraslanovsupan290@gmail.com>
2023-09-28 19:12:17 +0400

    Task complete

---
+++ Task28_09_23/Task28_09_23/Program.cs
@@ -0,0 +1,66 @@
+﻿// Task 1
+// int[] numbers = { 1, 2, 3, 4, 5 };
+
+// int sum = 0;
+
+// foreach (int number in numbers)
+// {
+//     sum += number;
+// }
+
+// Console.WriteLine($"The sum of the elements in the array is: {sum}");
+
+// Task 2
+// string[] words = { "cat", "dog", "fish", "bird", "lion", "wolf", "frog" };
+
+// Console.WriteLine("4-Letter Words:");
+
+// foreach (string word in words)
+// {
+//     if (word.Length == 4)
+//     {
+//         Console.WriteLine(word);
+//     }
+// }
+
+
+//Task 3 
+
+//int[] arr = new int[] {1, 3, 6, 7, 5, 9};
+ 
+//int temp;
+ 
+//for (int i = 0; i < arr.Length - 1; i++)
+ 
+//    for (int j = i + 1; j < arr.Length; j++)
+ 
+//        if (arr[i] < arr[j]) 
+//        {
+ 
+//            temp = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = temp;
+//        }
+ 
+        
+//foreach(int value in arr)
+//{
+//    Console.Write(value + " ");
+//}
+
+//Optional ;) dersde verdiyiniz 2D masivin butun elementlerini
+//       ekrana chixarir 
+
+int[,] arr2d = {
+                   {1, 2, 3},
+                   {3, 4, 5},
+                };
+for (int i = 0; i < arr2d.GetLength(0); i++)
+{
+    for (int j = 0; j < arr2d.GetLength(1); j++)
+    {
+        Console.Write(arr2d[i, j]);
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
